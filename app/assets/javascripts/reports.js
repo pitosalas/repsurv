364893a919ff3e3bfc036eaa0e7731d071a45b49
Pito Salas <pitosalas@gmail.com>
2012-11-21 15:19:29 -0500

    tiny css and js tweaks

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,15 +1,15 @@
-    $(function() {
-        /** This code runs when everything has been loaded on the page */
-        /* Inline sparklines take their values from the contents of the tag */
-        $('.inlinesparkline').sparkline('html', {type: 'bar',
-                    width: '100',
-                    height: '30',
-                    lineColor: '#ff5656',
-                    chartRangeMax: 5,
-                    chartRangeMin: 0});
+$(function() {
+    /** This code runs when everything has been loaded on the page */
+    /* Inline sparklines take their values from the contents of the tag */
+    $('.inlinesparkline').sparkline('html', {type: 'bar',
+                width: '100',
+                height: '30',
+                lineColor: '#ff5656',
+                chartRangeMax: 5,
+                chartRangeMin: 0});
 
-        $(".sparkpie").sparkline('html', {
-                    type: 'pie',
-                    height: '40'});
+    $(".sparkpie").sparkline('html', {
+                type: 'pie',
+                height: '40'});
 
-    });
+});
