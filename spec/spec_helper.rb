ff5b1973a87d93d8614b308d91c11eee095a2a09
Pito Salas <pitosalas@gmail.com>
2013-05-10 21:32:00 -0400

    rename requests to features in spec dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
 
-include Capybara::DSL 
-
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
